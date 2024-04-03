baf70d2152a670edd97b337b0c7ec4046af51edd
Choi TaeSan <xotks7524@gmail.com>
2024-04-03 22:48:33 +0900

    first commit

---
+++ src/main/java/org/example/TestController.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class TestController {
+    @GetMapping("/test")
+    public String test(){
+        return "Hello World!";
+    }
+}
