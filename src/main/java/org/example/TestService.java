cf3b5e64535568207266f03554a01d5eb713e3eb
Choi TaeSan <xotks7524@gmail.com>
2024-04-05 22:56:06 +0900

    first commit

---
+++ src/main/java/org/example/TestService.java
@@ -0,0 +1,13 @@
+package org.example;
+
+import org.springframework.beans.factory.annotation.Autowired;
+
+import java.util.List;
+
+public class TestService {
+    @Autowired
+    MemberRepository memberRepository; // 빈 주입
+    public List<Member> getAllMember() {
+        return memberRepository.findAll(); // 멤버 목록 얻기
+    }
+}
