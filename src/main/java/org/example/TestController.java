bf9a74fb68e3927bb719a549a1cfc0485796e625
Choi TaeSan <xotks7524@gmail.com>
2024-06-02 14:09:04 +0900

    Feat: 멤버 목록 얻기 추가

--- src/main/java/org/example/TestController.java
+++ src/main/java/org/example/TestController.java
@@ -4,13 +4,16 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
 @RestController
 public class TestController {
     @Autowired
     TestService testService;
 
     @GetMapping("/test")
-    public String test(){
-        return "Hello World!";
+    public List<Member> getAllMember(){
+        List<Member> members = testService.getAllMember();
+        return members;
     }
 }
